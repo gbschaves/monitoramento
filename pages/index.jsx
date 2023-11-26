2cfbd364b78781496c5afe9d47665ba00463eb1a
Guilherme Chaves <53453346+gbschaves@users.noreply.github.com>
2023-11-26 14:44:36 -0300

    Update index.jsx test 2

--- pages/index.jsx
+++ pages/index.jsx
@@ -80,7 +80,7 @@ const SpotsPage = () => {
         };
 
         const response = await fetch(
-          "http://mercadominio.websac.net/v3/api/relatorio/32",
+          "https://mercadominio.websac.net/v3/api/relatorio/32",
           requestOptions
         );
 
